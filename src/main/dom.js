e2df7bd01294f03a3bbfa302427cc2491e503a8c
anoldrookie <1105094775@qq.com>
2023-06-03 14:12:49 +0800

    add

--- src/main/dom.js
+++ src/main/dom.js
@@ -1,14 +1,14 @@
 // import {BoxBottonButton} from '../main/main.js'
-// const texture = document.getElementById('texture')
+const MapTexture = document.getElementById('texture')
 
 
 let flag_bg_texture_box = true
 function TogglePanel(){
     if(flag_bg_texture_box){
-        texture.style.display='block'
+        MapTexture.style.display='block'
         flag_bg_texture_box = false
     }else{
-        texture.style.display='none'
+        MapTexture.style.display='none'
         flag_bg_texture_box= true
     }
 }
